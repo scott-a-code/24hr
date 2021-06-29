f8ba58a203b52768229ee9fef97f5ecc3e61ea9b
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-06-29 15:09:17 +0100

    data structure defined

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 
+// Import our model
+const AnimeShow = require('./models/anime')
+
+const temp = AnimeShow.all
+console.log(temp)
+
 const port = 3000
+
 
 app.listen(port, () => {
     console.log(`listening on port: ${port}`)
---
+++ server/models/anime.js
@@ -0,0 +1,13 @@
+const animeData = require('../../data')
+
+class AnimeShow {
+    constructor(data) {
+        // Attributes and methods go here
+    }
+    // Static methods go here
+    static get all() {
+        return 'hi'
+    }
+}
+
+module.exports = AnimeShow
