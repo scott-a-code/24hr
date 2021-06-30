46f551e746fc45470f9724ea3c68277d3dbb5b6f
Scott <scott-a-ryan@hotmail.co.uk>
2021-06-30 12:40:14 +0100

    updated data file

--- data.js
+++ data.js
@@ -13,16 +13,112 @@ const animeData = [
     },
     {
         id: 3,
-        name: 'Fullmetal Alchemist: Brotherhood',
-        metaData: [ 'ACTION', 'MILITARY', 'ADVENTURE',  'DRAMA', 'MAGIC', 'FANTASY', 'SHOUNEN'],
-        url: 'https://www.hagaren.jp/fa/'
+        name: 'Naruto: Shippuuden',
+        metaData: [ 'ACTION', 'ADVENTURE', 'COMEDY',  'SUPER POWER', 'MARTICAL ARTS', 'SHOUNEN'],
+        url: 'https://www.viz.com/naruto'
     },
     {
         id: 4,
-        name: 'Hi',
-        metaData: ['Oops'],
-        url: 'https://www.hagaren.jp/fa/'
+        name: 'Steins;Gate',
+        metaData: ['SCI-FI', 'PSYCHOLOGICAL' , 'DRAMA', 'THRILLER'],
+        url: 'https://steins-gate.us/'
     },
+    {
+        id: 5,
+        name: 'Hunter X Hunter',
+        metaData: ['ACTION', 'ADVENTURE' , 'FANTASY', 'SHOUNEN', 'SUPER POWER'],
+        url: 'https://www.viz.com/hunter-x-hunter'
+    },
+    {
+        id: 6,
+        name: 'Attack on Titan',
+        metaData: ['ACTION', 'MILITARY' , 'MYSTERY', 'SUPER POWER','DRAMA', 'FANTASY','SHOUNEN'],
+        url: 'https://www.koeitecmoamerica.com/attackontitan/'
+    },
+    {
+        id: 7,
+        name: 'Code Geass',
+        metaData: ['ACTION', 'MILITARY' , 'SCI-FI', 'SUPER POWER', 'DRAMA', 'MECHA'],
+        url: 'http://www.geass.jp/'
+    },
+    {
+        id: 8,
+        name: 'Mob Psycho 100',
+        metaData: ['ACTION', 'SLICE OF LIFE' , 'COMEDY', 'SUPERNATURAL'],
+        url: 'http://mobpsycho100.com/'
+    },
+    {
+        id: 9,
+        name: 'Fate/Stay night',
+        metaData: ['ACTION', 'SUPERNATURAL' , 'MAGIC', 'FANTASY'],
+        url: 'https://www.fatestaynightusa.com/ubw/'
+    },
+    {
+        id: 10,
+        name: 'Jujutsu Kaisen',
+        metaData: ['ACTION', 'DEMONS' , 'SUPERNATURAL', 'SCHOOL', 'SHOUNEN'],
+        url: 'https://www.viz.com/jujutsu-kaisen'
+    },
+    {
+        id: 11,
+        name: 'Demon Slayer',
+        metaData: ['ACTION', 'HISTORICAL' , 'DEMONS', 'SUPERNATURAL', 'SHOUNEN'],
+        url: 'https://demonslayer-anime.com/'
+    },
+    {
+        id: 12,
+        name: 'Vinland Saga',
+        metaData: ['ACTION', 'ADVENTURE' , 'DRAMA', 'HISTORICAL', 'SEINEN'],
+        url: 'https://vinlandsaga.jp/'
+    },
+    {
+        id: 13,
+        name: 'Made in Abyss',
+        metaData: ['SCI-FI', 'ADVENTURE' , 'MYSTERY', 'DRAMA', 'FANTASY'],
+        url: 'http://miabyss.com/'
+    },
+    {
+        id: 14,
+        name: 'Death Note',
+        metaData: ['MYSTERY', 'POLICE' , 'PSYCHOLOGICAL', 'SUPERNATURAL', 'THRILLER', 'SHOUNEN'],
+        url: 'https://www.viz.com/death-note'
+    },
+    {
+        id: 15,
+        name: 'Hajime no Ippo',
+        metaData: ['COMEDY', 'SPORTS' , 'DRAMA', 'SHOUNEN'],
+        url: 'https://hajime-noippo.com/'
+    },
+    {
+        id: 16,
+        name: 'One Piece',
+        metaData: ['ACTION', 'ADVENTURE' , 'COMEDY', 'SUPER POWER', 'DRAMA', 'FANTASY', 'SHOUNEN'],
+        url: 'https://onepieceofficial.com/'
+    },
+    {
+        id: 17,
+        name: 'Toriko',
+        metaData: ['ACTION', 'ADVENTURE' , 'COMEDY', 'FANTASY', 'SHOUNEN'],
+        url: 'https://www.viz.com/toriko'
+    },
+    {
+        id: 18,
+        name: 'One Punch Man',
+        metaData: ['ACTION', 'SCI-FI', 'COMEDY' , 'PARODY', 'SUPER POWER', 'SUPERNATURAL'],
+        url: 'https://www.viz.com/one-punch-man'
+    },
+    {
+        id: 19,
+        name: 'Psycho-pass',
+        metaData: ['ACTION', 'SCI-FI', 'POLICE' , 'PSYCHOLOGICAL', 'THRILLER'],
+        url: 'https://psycho-pass.com/'
+    },
+    {
+        id: 20,
+        name: 'Mobile Suit Gundam: Iron-Blooded Orphans',
+        metaData: ['ACTION', 'DRAMA', 'MECHA' , 'SCI-FI', 'SPACE'],
+        url: 'https://en.gundam.info/series/tekketsu/'
+    }
 ]
 
 module.exports = animeData
